766fec2262b66927c1d45d21e647657d95045bc7
axz <axz_zhang@gmail.com>
2024-04-24 00:59:52 -0500

    lmao

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,2 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-function fish_greeting; end
-
-alias ls='exa -la'
-set -x TERM xterm-256color
+set fish_greeting ''
+alias ls 'eza -la'
